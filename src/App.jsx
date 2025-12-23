ce41d80eb1bde1d39a4c911ce7f25ab477897fb1
Olga Sessions <olenkafirst@gmail.com>
2025-12-23 14:55:44 -0500

    updated list

--- src/App.jsx
+++ src/App.jsx
@@ -6,13 +6,24 @@ import TodoList from "./TodoList.jsx";
 function App() {
   const [todoList, setTodoList] = useState([]);
   function addTodo(title) {
-    const newTodo = { title, id: Date.now() };
+    const newTodo = { title, id: Date.now(), isCompleted: false };
     setTodoList([...todoList, newTodo]);
   }
+  function completeTodo(id) {
+    const updatedTodos = todoList.map((todo) => {
+      if (todo.id === id) {
+        return { ...todo, isCompleted: true };
+      } else {
+        return todo;
+      }
+    });
+    setTodoList(updatedTodos);
+  }
+
   return (
     <div>
       <TodoForm onAddTodo={addTodo} />
-      <TodoList todoList={todoList} />
+      <TodoList todoList={todoList} onCompleteTodo={completeTodo} />
     </div>
   );
 }
--- src/TodoListItem.jsx
+++ src/TodoListItem.jsx
@@ -1,7 +1,18 @@
-export default function TodoListItem({ todo }) {
+export default function TodoListItem({ todo, onCompleteTodo }) {
   return (
     <>
-      <li>{todo.title}</li>
+      <li>
+        <form>
+          <input
+            type="checkbox"
+            defaultChecked={false}
+            onChange={() => {
+              onCompleteTodo(todo.id);
+            }}
+          />
+          {todo.title}
+        </form>
+      </li>
     </>
   );
 }
