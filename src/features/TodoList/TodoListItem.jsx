21356ff985bb5849ce04ceb1ea4055808c440520
Olga Sessions <olenkafirst@gmail.com>
2026-01-06 15:20:29 -0500

    updated list

--- src/features/TodoList/TodoListItem.jsx
+++ src/features/TodoList/TodoListItem.jsx
@@ -1,18 +1,65 @@
-export default function TodoListItem({ todo, onCompleteTodo }) {
+import { useState } from "react";
+import TextInputWithLabel from "../../shared/TextInputWithLabel";
+function TodoListItem({ todo, onCompleteTodo, onUpdateToDo }) {
+  const [isEditing, setIsEditing] = useState(false);
+  const [workingTitle, setWorkingTitle] = useState(todo.title);
+
+  function handleUpdate(event) {
+    if (!isEditing) return;
+    event.preventDefault();
+    onUpdateToDo({ ...todo, title: workingTitle });
+
+    setIsEditing(false);
+  }
+  const handleCancel = () => {
+    setWorkingTitle(todo.title);
+    setIsEditing(false);
+  };
+  const handleEdit = (event) => {
+    setWorkingTitle(event.target.value);
+  };
+  const updateEdit = () => {
+    setIsEditing(true);
+  };
   return (
-    <>
-      <li>
-        <form>
-          <input
-            type="checkbox"
-            defaultChecked={false}
-            onChange={() => {
-              onCompleteTodo(todo.id);
-            }}
-          />
-          {todo.title}
-        </form>
-      </li>
-    </>
+    <li>
+      <form>
+        {isEditing ? (
+          <>
+            <TextInputWithLabel value={workingTitle} onChange={handleEdit} />
+            <button type="button" onClick={handleCancel}>
+              Cancel
+            </button>
+            <button type="button" onClick={handleUpdate}>
+              Update
+            </button>
+          </>
+        ) : (
+          <>
+            <label>
+              <input
+                type="checkbox"
+                id={`checkbox${todo.id}`}
+                checked={todo.isCompleted}
+                onChange={() => {
+                  onCompleteTodo(todo.id);
+                }}
+              />
+            </label>
+            <span onClick={() => setIsEditing(true)}>{todo.title}</span>
+            {/* <button
+              type="button"
+              onClick={() => {
+                setIsEditing(true);
+              }}
+            >
+              Edit
+            </button> */}
+          </>
+        )}
+      </form>
+    </li>
   );
 }
+
+export default TodoListItem;
